156d679fcfe0e7af4316eec227ce6bf3b6c75326
Andrei Sorescu <and.sorescu@gmail.com>
2021-06-13 18:32:03 +0300

    * added IE11 support

--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,33 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
+          test: /\.(js|jsx|ts|tsx)$/,
           exclude: /node_modules/,
-          use: ["babel-loader"],
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    corejs: {
+                      version: 3,
+                    },
+                    useBuiltIns: "usage",
+                    targets: {
+                      edge: "17",
+                      firefox: "60",
+                      chrome: "67",
+                      safari: "11.1",
+                      ie: "11",
+                    },
+                  },
+                ],
+                "@babel/react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
         },
         {
           test: /\.(ts|tsx)$/,
